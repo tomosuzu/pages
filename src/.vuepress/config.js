4884a5b019289af71a38c6479b9fb047fba67c25
tomosuzu <tomoya_su40@hotmail.com>
2019-07-09 17:41:26 +0900

    add ga

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -3,12 +3,16 @@ module.exports = {
   description: 'tomosuzu\'s page',
   dest: 'docs/',
   base: '/practices/',
-  ga: 'UA-37628540-15',
   themeConfig: {
     locales: {
       '/': {
         nav: require('./nav/ja'),
       }
     }
-  }
+  },
+  plugins: [
+    ['@vuepress/google-analytics', {
+      ga: 'UA-37628540-15'
+    }]
+  ]
 }
