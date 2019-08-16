5a9defc189ac00fb377a29ee70849d27b240d586
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-16 09:18:40 +0000

    Deploy tomosuzu/pages to github.com/tomosuzu/pages.git:master

--- src/.vuepress/nav/ja.js
+++ src/.vuepress/nav/ja.js
@@ -8,6 +8,10 @@ module.exports = [
     link: '/practices/',
   },
   {
+    text: 'ブログ',
+    link: '/blog/',
+  },
+  {
     text: 'フィードバック',
     link: 'https://forms.gle/TKUJ2Gs9EoH2jQvp7',
   }
