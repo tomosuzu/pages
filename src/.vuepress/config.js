048e49d03a6f10cc4028595a5ba195e0f101c7a6
Tomoya Suzuki <tosuzuki@redhat.com>
2020-05-07 20:55:03 +0900

    move

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -10,86 +10,9 @@ module.exports = {
       }
     },
     sidebar: {
-      '/practices/' : [
-        {
-          title: 'プラクティス一覧',
-          collapsable: false,
-          sidebarDepth: 2,
-          children: [
-            '/practices/'
-          ]
-        },
-        {
-          title: 'Discovery',
-          collapsable: false,
-          sidebarDepth: 2,
-          children: [
-            'impact-mapping',
-            'user-story-mapping',
-            'value-stream-process-mapping',
-            'empathy-mapping'
-          ]
-        },
-        {
-          title: 'Options/Pivot',
-          collapsable: false,
-          sidebarDepth: 2,
-          children: [
-            'backlog-refinement'
-          ]
-        },
-        {
-          title: 'Delivery',
-          collapsable: false,
-          sidebarDepth: 2,
-          children: [
-            'daily-standup',
-            'iteration-planning',
-            'retrospectives'
-          ]
-        },
-        {
-          title: 'Foundation',
-          collapsable: false,
-          sidebarDepth: 2,
-          children: [
-            'pair-programming',
-            'mob-programming',
-            'test-driven-development',
-            'social-contract',
-            'team-sentiment',
-            'continuous-integration'
-          ]
-        }
-      ],
-      '/blog/' : [
-        {
-          title: '201908',
-          collapsable: false,
-          sidebarDepth: 2,
-          children: [
-            'JobsToBeDone1'
-          ]
-        }
-      ],
-      '/training/' : [
-        {
-          title: '筋トレ',
-          collapsable: false,
-          sidebarDepth: 2,
-          children: [
-            '/training/'
-          ]
-        },
-        {
-          title: '過去の記録',
-          collapsable: false,
-          sidebarDepth: 0,
-          children: [
-            '202004'
-          ]
-        }
-      ]
+      '/practices/' : require('./side/ja_practices'),
+      '/blog/'      : require('./side/ja_blog'),
+      '/training/'  : require('./side/ja_training')
     }
   },
   plugins: [
---
+++ src/.vuepress/side/ja_training.js
@@ -0,0 +1,11 @@
+module.exports = [
+  {
+    title: '体の鍛錬',
+    collapsable: false,
+    sidebarDepth: 2,
+    children: [
+      '/training/workout/',
+      '/training/workout/202004'
+    ]
+  }
+]
