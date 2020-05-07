8ab0dba788692f8ee3a14c27d92cfe1fcc54cdad
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-07 11:56:50 +0000

    Deploy tomosuzu/pages to github.com/tomosuzu/pages.git:master

---
+++ src/.vuepress/side/ja_blog.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    title: '2020',
+    collapsable: false,
+    sidebarDepth: 2,
+    children: [
+      '2020/コーチングのすべて'
+    ]
+  },
+  {
+    title: '2019',
+    collapsable: false,
+    sidebarDepth: 2,
+    children: [
+      '2019/JobsToBeDone1'
+    ]
+  }
+]
---
+++ src/.vuepress/side/ja_practices.js
@@ -0,0 +1,52 @@
+module.exports = [
+  {
+    title: 'プラクティス一覧',
+    collapsable: false,
+    sidebarDepth: 2,
+    children: [
+      '/practices/'
+    ]
+  },
+  {
+    title: 'Discovery',
+    collapsable: false,
+    sidebarDepth: 2,
+    children: [
+      'impact-mapping',
+      'user-story-mapping',
+      'value-stream-process-mapping',
+      'empathy-mapping'
+    ]
+  },
+  {
+    title: 'Options/Pivot',
+    collapsable: false,
+    sidebarDepth: 2,
+    children: [
+      'backlog-refinement'
+    ]
+  },
+  {
+    title: 'Delivery',
+    collapsable: false,
+    sidebarDepth: 2,
+    children: [
+      'daily-standup',
+      'iteration-planning',
+      'retrospectives'
+    ]
+  },
+  {
+    title: 'Foundation',
+    collapsable: false,
+    sidebarDepth: 2,
+    children: [
+      'pair-programming',
+      'mob-programming',
+      'test-driven-development',
+      'social-contract',
+      'team-sentiment',
+      'continuous-integration'
+    ]
+  }
+]
