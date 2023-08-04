9f4d4a7297705a39dfa28573a3d2dba20b0d826c
tomosuzu <tomoya_su40@hotmail.com>
2023-08-04 12:50:48 +0900

    modify

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,20 +1,90 @@
+import { defaultTheme } from '@vuepress/theme-default'
+
 module.exports = {
   title: 'tomosuzu\'s pages',
   description: 'produced by tomosuzu',
   dest: 'docs/',
   base: '/',
-  themeConfig: {
-    locales: {
-      '/': {
-        nav: require('./nav/ja'),
-      }
-    },
-    sidebar: {
-      '/practices/' : require('./side/ja_practices'),
-      '/blog/'      : require('./side/ja_blog'),
-      '/training/'  : require('./side/ja_training')
+  locales: {
+    '/': {
+      lang: 'ja-JP',
     }
   },
+  theme: defaultTheme({
+    navbar: [
+      {
+        text: '当サイトについて',
+        link: '/about/',
+      },
+      {
+        text: 'スクラムの教科書',
+        link: '/scrum/',
+      },
+      {
+        text: 'プラクティス一覧',
+        link: '/practices/',
+      },
+      {
+        text: '雑記',
+        link: '/blog/',
+      },
+      {
+        text: '問い合わせ',
+        link: 'https://forms.gle/TKUJ2Gs9EoH2jQvp7',
+      }
+    ],
+    sidebar: {
+      '/scrum/': [
+        {
+          text: 'about',
+          children: ['/scrum/README.md'],
+        },
+      ],
+      '/practices/': [
+        {
+          text: 'about',
+          children: ['/practices/README.md'],
+        },
+        {
+          text: 'Discovery',
+          children: [
+            'impact-mapping',
+            'user-story-mapping',
+            'value-stream-process-mapping',
+            'empathy-mapping'
+          ],
+        },
+        {
+          text: 'Options/Pivot',
+          children: [
+            'backlog-refinement'
+          ],
+        },
+        {
+          text: 'Delivery',
+          children: [
+            'daily-standup',
+            'iteration-planning',
+            'retrospectives'
+          ],
+        },
+        {
+          text: 'Foundation',
+          children: [
+            'pair-programming',
+            'mob-programming',
+            'test-driven-development',
+            'social-contract',
+            'team-sentiment',
+            'visualisation-of-work',
+            'continuous-integration',
+            'continuous-delivery',
+            'continuous-deployment'
+          ],
+        },
+      ],
+    },
+  }),
   plugins: [
     ['@vuepress/google-analytics', {
       ga: 'UA-37628540-15'
--- src/en/about/README.md
+++
@@ -1,8 +0,0 @@
-# about
-
-## Author
-I'm tomosuzu, Software Engineer and DevOps/Agile Consultant.
-
-## about this site
-
-## Privacy Policy
